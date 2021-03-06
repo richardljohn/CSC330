043d6a0984ceb3fff5921e5552e056a567199cd3
richardljohn <richardljohn@gmail.com>
2021-03-06 15:06:30 -0500

    Inheritence Done

--- Inheritence/Customers/src/PreferredCustomer.java
+++ Inheritence/Customers/src/PreferredCustomer.java
@@ -3,9 +3,15 @@ public class PreferredCustomer extends Customer {
     private double cummulativePurchases;
     private double discount;
 
-    PreferredCustomer(){
+    PreferredCustomer() {
         cummulativePurchases = 0.0;
         discount = 0.0;
+    }
+
+    PreferredCustomer(String n, String a, String p, int c, boolean w, double cu, double d){
+        super(n, a, p, c, w);
+        cummulativePurchases = cu;
+        discount = d;
     }
 
     public double getCummulativePurchases(){
@@ -25,20 +31,31 @@ public class PreferredCustomer extends Customer {
     }
 
     public void determineDiscount(){
+        double discount = 0.0;
         if(cummulativePurchases < 500){
             discount = 0.0;
+            setDiscount(discount);
         }
         else if(cummulativePurchases == 500){
             discount = 0.05;
+            setDiscount(discount);
         }
         else {
             double total = getCummulativePurchases() - 500.0;
             discount = 0.05;
             while(total >= 500){
                 total = total - 500.0;
-                discount += 0.01;
+                discount = discount + 0.01;
+
             }
+            setDiscount(discount);
         }
+    }
+
+    public String toString() {
+        return super.toString() +
+                "\nCummulative Purchases: " + getCummulativePurchases() +
+                "\nDiscount: " + getDiscount();
     }
     
 }
--- Inheritence/Customers/src/PreferredCustomerRunner.java
+++ Inheritence/Customers/src/PreferredCustomerRunner.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
+import java.util.prefs.PreferencesFactory;
+
 public class PreferredCustomerRunner {
     public static void main(String[] args){
-
+        PreferredCustomer Timmothy = new PreferredCustomer();
+        System.out.println(Timmothy);
+        Scanner input = new Scanner(System.in);
+        String name_, address_, phone, want;
+        double cummulative, disc;
+        boolean interested;
+        int cNum = 2;
+        System.out.print("Enter your name: ");
+        name_ = input.nextLine();
+        System.out.print("Enter your address: ");
+        address_ = input.nextLine();
+        System.out.print("Enter your phone number: ");
+        phone = input.nextLine();
+        System.out.print("Are you interested in signing up for the waitlist? Enter y for yes and n for no: ");
+        want = input.nextLine();
+        if(want.toLowerCase().equals("n")){
+            interested = false;
+        } else {
+            interested = true;
+        }
+        System.out.print("Enter how much you spent: ");
+        cummulative = input.nextDouble();
+        disc = 0.0;
+        PreferredCustomer NotKaren = new PreferredCustomer(name_, address_, phone, cNum, interested, cummulative, 0.0);
+        NotKaren.determineDiscount();
+        System.out.println(NotKaren);
     }
 }
+
+//    public double determineDiscount(double cummul){
+//        double discount = 0.0;
+//        if(cummul < 500){
+//            discount = 0.0;
+//        }
+//        else if(cummul == 500){
+//            discount = 0.05;
+//        }
+//        else {
+//            double total = cummul - 500.0;
+//            discount = 0.05;
+//            while(total >= 500){
+//                total = total - 500.0;
+//                discount += 0.01;
+//            }
+//        }
+//        return discount;
+//    }
