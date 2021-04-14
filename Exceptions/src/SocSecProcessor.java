1ef590810cac7f7afb5b2700a518e667c573d43e
richardljohn <richardljohn@gmail.com>
2021-04-14 00:11:05 -0400

    Getting throw exceptions almost done

--- Exceptions/src/SocSecProcessor.java
+++ Exceptions/src/SocSecProcessor.java
@@ -22,12 +22,23 @@ public class SocSecProcessor {
 
     public static boolean isValid(String ss) throws SocSecException {
         if(ss.length() != 11){
-            throw new SocSecException("Invalid social security number, wrong number of characters");
+            throw new SocSecException("Invalid social security number, wrong number of " +
+                    "\ncharacters");
         }
         for(int i = 0; i < ss.length(); i++){
-            if(ss.charAt(i) < 48 && ss.charAt(i) > 57){
-                throw new SocSecException("Invalid social security number")
+            if(i == 3 || i == 6){
+                if(ss.charAt(i) != '-'){
+                    throw new SocSecException("Invalid social security number, no dash in "
+                            + "\nexpected space.");
+                }
             }
+            if(i != 3 && i != 6) {
+                if (ss.charAt(i) < 48 && ss.charAt(i) > 57) {
+                    throw new SocSecException("Invalid social security number, contains a " +
+                            "\ncharacter that is not a digit");
+                }
+            }
+
         }
         return false;
     }
